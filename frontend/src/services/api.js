17980af855a0acf206fed55b699eb7c8fbe71c0a
DELL <roksolianakachala@gmail.com>
2026-05-05 22:34:09 +0300

    Fix API service bug

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,6 @@
 const DEFAULT_API_BASE_URL = "https://eventra-j1tj.onrender.com/api";
 
-// "https://eventra-j1tj.onrender.com/api"
-// = "http://localhost:5000/api"
+
 
 export const API_BASE_URL = (
   process.env.REACT_APP_API_URL || DEFAULT_API_BASE_URL
