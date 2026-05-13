5e31f8c05efba427692233f60d1473eb7706c428
Arsen Tabachyshyn <arsentabachishin@gmail.com>
2026-05-13 10:22:56 +0300

    error: CORS violation
    cannot access to data

--- backend/server.js
+++ backend/server.js
@@ -12,13 +12,23 @@ app.get("/health", (req, res) => {
   res.send("ok");
 });
 
-app.use(cors({
-  origin: [
-    "http://localhost:3000",
-    "https://eventra-for-events.netlify.app"
-  ],
-  credentials: true
-}));
+app.use(cors({ 
+  origin: function (origin, callback) {
+    const allowedOrigins = [
+      "http://localhost:3000",
+      "https://eventra-for-events.netlify.app"
+    ]; 
+    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+})); 
+
 app.use(express.json()); 
 
 app.get('/', (req, res) => {
