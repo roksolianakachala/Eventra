f0d1bb983866cd31222c85a6f1fd414e2b78c34a
Arsen Tabachyshyn <arsentabachishin@gmail.com>
2026-05-16 19:28:03 +0300

    AT2-43: createEvent
    added banner storage

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,8 @@ app.use(cors({
   ],
   credentials: true
 }));
-app.use(express.json()); 
+app.use(express.json({ limit: '10mb' })); 
+app.use(express.urlencoded({ limit: '10mb', extended: true }));
 
 app.get('/', (req, res) => {
   res.send('running ');
